b35d83a7c8346ec5e7adb9558b4d02e00d0c8ab0
Patricia <patrihernandezperez9@gmail.com>
2024-12-04 20:31:27 +0100

    funciones ft

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 15:02:15 by pahernan          #+#    #+#             */
+/*   Updated: 2024/12/02 15:15:53 by pahernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	j = 0;
+	i = 0;
+	while ((dest[i] != '\0'))
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (i);
+}
