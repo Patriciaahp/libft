be7869e6d6eb251fc6402440f6897e360d2d693a
Patriciaahp <patrihernandezperez9@gmail.com>
2024-12-28 20:53:07 +0100

    split falta menos lineas, norminette ft_strlcpy.c

--- ft_split.c
+++ ft_split.c
@@ -24,28 +24,43 @@ char	**ft_split(char const *s, char c)
 	l = 0;
 	while (s[i])
 		i++;
-	s3 = (char**)malloc (i * i);
+	s3 = (char **)malloc((i + 1) * sizeof(char *));
+	if (!s3)
+		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			i++;
 			k++;
+			l = 0;
+			i++;
 		}
-		s3[k] = (char**)malloc (i * i);
+		if (l == 0)
+			s3[k] = (char *)malloc((i + 1) * sizeof(char));
 		s3[k][l] = s[i];
 		l++;
 		i++;
 	}
-	free(s3);
+	s3[k][l] = '\0';
+	s3[k + 1] = NULL;
 	return (s3);
 }
-#include <stdio.h>
-int main (void)
+
+/*#include <stdio.h>
+int main(void)
 {
-	char *s = "holazholazholazhola";
-	char c = 'z';
-	printf("%s\n", ft_split(s, c));
-	return (0);
-}
+    char *s = "holazholazholazhola";
+    char c = 'z';
+
+    char **result = ft_split(s, c);
+
+
+    for (int i = 0; result[i] != NULL; i++) {
+        printf("Subcadena %d: %s\n", i, result[i]);
+        free(result[i]);
+    }
+    free(result);
+
+    return 0;
+}*/
