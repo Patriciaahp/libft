0cbacd2701335ad95bbefae992668e4bf5b48127
Patriciaahp <patrihernandezperez9@gmail.com>
2024-12-31 08:48:35 +0100

    arreglado ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -17,11 +17,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	int	i;
 
 	i = 0;
-	while ((((char *)s1)[i] != 0 || ((char *)s2)[i] != 0) && n > 0)
+	while ((((unsigned char *)s1)[i] != 0 || ((unsigned char *)s2)[i] != 0) && n > 0)
 	{
-		if (((char *)s1)[i] != ((char *)s2)[i])
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
 		{
-			return (((char *)s1)[i] - ((char *)s2)[i]);
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		}
 		i++;
 		n--;
