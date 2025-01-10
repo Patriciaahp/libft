d359e3572e3c0261ec3b13955b6d97fb285f11ab
Patriciaahp <patrihernandezperez9@gmail.com>
2025-01-10 10:33:47 +0100

    itoa a medias

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,9 +12,9 @@
 
 #include "libft.h"
 
-static int ft_numlen(int n)
+static int	ft_numlen(int n)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (n == 0)
@@ -31,11 +31,11 @@ static int ft_numlen(int n)
 	return (i);
 }
 
-char *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char *s;
-	int i;
-	int j;
+	char	*s;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
@@ -44,10 +44,11 @@ char *ft_itoa(int n)
 		s = "-2147483648";
 		return (s);
 	}
-	if (n == 0)
-		s[0] = '0';
+
 	i = ft_numlen(n);
 	s = malloc(sizeof(char) * (i + 1));
+	if (n == 0)
+	s[0] = '0';
 	j = i;
 	if (n < 0)
 	{
@@ -78,89 +79,6 @@ char *ft_itoa(int n)
 #include <stdio.h>
 int main(void)
 {
-	int n = 0;
-	printf("%s\n", ft_itoa(n));
-}
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/19 14:06:39 by pahernan          #+#    #+#             */
-/*   Updated: 2025/01/09 12:58:11 by pahernan         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-static int ft_numlen(int n)
-{
-	int i;
-
-	i = 0;
-	if (n == 0)
-		return (1);
-	if (n < 0)
-	{
-		i++;
-	}
-	while (n)
-	{
-		n /= 10;
-		i++;
-	}
-	return (i);
-}
-
-char *ft_itoa(int n)
-{
-	char *s;
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	if (n == -2147483648)
-	{
-		s = "-2147483648";
-		return (s);
-	}
-	if (n == 0)
-		s[0] = '0';
-	i = ft_numlen(n);
-	s = malloc(sizeof(char) * (i + 1));
-	j = i;
-	if (n < 0)
-	{
-		n = -n;
-		s[0] = '-';
-	}
-
-	while (i > 0)
-	{
-		if (s[0] == '-')
-		{
-			i--;
-			s[i] = (n % 10) + '0';
-			n /= 10;
-			s[0] = '-';
-		}
-
-		i--;
-		s[i] = (n % 10) + '0';
-		n /= 10;
-	}
-	s[j] = '\0';
-	if (!s)
-		return (NULL);
-	return (s);
-}
-
-#include <stdio.h>
-int main(void)
-{
-	int n = 0;
+	int n = -8;
 	printf("%s\n", ft_itoa(n));
 }
