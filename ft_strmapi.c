b50ce4402557cbb5ce3b3061e7123d89bacb9178
Patricia <patrihernandezperez9@gmail.com>
2025-01-10 19:50:41 +0100

    hecho ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pahernan <pahernan@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 19:39:02 by pahernan          #+#    #+#             */
+/*   Updated: 2025/01/10 19:39:02 by pahernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *str;
+	int i;
+
+	i = 0;
+	if (!s || !f)
+		return (NULL);
+	while (s[i])
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	if (!str)
+		return (NULL);
+	str[i] = '\0';
+	return (str);
+}
