48d9aa6742e3285dd95342a79488e38a9566e56f
Patriciaahp <patrihernandezperez9@gmail.com>
2025-01-17 19:17:44 +0100

    añadido ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -9,3 +9,21 @@
 /*   Updated: 2025/01/17 19:01:50 by pahernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	if (!lst)
+		return (0);
+	while (lst)
+	{
+		i++;
+		lst = lst->next;
+	}
+
+	return (i);
+}
